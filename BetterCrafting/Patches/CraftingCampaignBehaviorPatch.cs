e3b675ab5c9bb6a4312af5a2f7820851ce94335c
Shadow <Shadow32055@gmail.com>
2022-07-15 13:23:19 -0400

    Update CraftingCampaignBehaviorPatch.cs

--- BetterCrafting/Patches/CraftingCampaignBehaviorPatch.cs
+++ BetterCrafting/Patches/CraftingCampaignBehaviorPatch.cs
@@ -5,15 +5,18 @@ using System.Reflection;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.CampaignBehaviors;
 
-namespace BetterCrafting.Patches {
+namespace BetterCrafting.Patches
+{
 
     [HarmonyPatch(typeof(CraftingCampaignBehavior))]
-    class CraftingCampaignBehaviorPatch {
+    class CraftingCampaignBehaviorPatch
+    {
         private static FieldInfo? recordsInfo;
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(CraftingCampaignBehavior), "HourlyTick")]
-        static bool Prefix(ref CraftingCampaignBehavior __instance) {
+        static bool Prefix(ref CraftingCampaignBehavior __instance)
+        {
             if (recordsInfo == null)
                 recordsInfo = typeof(CraftingCampaignBehavior).GetField("_heroCraftingRecords", BindingFlags.Instance | BindingFlags.NonPublic);
 
@@ -21,10 +24,12 @@ namespace BetterCrafting.Patches {
 
             IDictionary? records = (IDictionary)recordsInfo.GetValue(__instance);
 
-            foreach (Hero hero in records.Keys) {
+            foreach (Hero hero in records.Keys)
+            {
                 int curCraftingStamina = __instance.GetHeroCraftingStamina(hero);
 
-                if (curCraftingStamina < __instance.GetMaxHeroCraftingStamina(hero)) {
+                if (curCraftingStamina < __instance.GetMaxHeroCraftingStamina(hero))
+                {
                     int staminaGainAmount = 5; //Native is 5?
                     float CraftingStaminaGainOutsideSettlementMultiplier = .05f;
                     if (CraftingStaminaGainOutsideSettlementMultiplier < 1 && hero.PartyBelongedTo?.CurrentSettlement == null)
@@ -41,16 +46,18 @@ namespace BetterCrafting.Patches {
         }
 
         [HarmonyPostfix]
-		[HarmonyPatch(typeof(CraftingCampaignBehavior), nameof(CraftingCampaignBehavior.GetHeroCraftingStamina))]
-		public static void GetHeroCraftingStamina(ref int __result, ref CraftingCampaignBehavior __instance, Hero hero) {
-			__result = __instance.GetMaxHeroCraftingStamina(hero);
+        [HarmonyPatch(typeof(CraftingCampaignBehavior), nameof(CraftingCampaignBehavior.GetHeroCraftingStamina))]
+        public static void GetHeroCraftingStamina(ref int __result, ref CraftingCampaignBehavior __instance, Hero hero)
+        {
+            __result = __instance.GetMaxHeroCraftingStamina(hero);
         }
 
-		[HarmonyPostfix]
-		[HarmonyPatch(typeof(CraftingCampaignBehavior), nameof(CraftingCampaignBehavior.GetMaxHeroCraftingStamina))]
-		public static void GetMaxHeroCraftingStamina(ref int __result, ref CraftingCampaignBehavior __instance, Hero hero) {
-			if (hero == Hero.MainHero)
-				__result = 1000;
-		}
-	}
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(CraftingCampaignBehavior), nameof(CraftingCampaignBehavior.GetMaxHeroCraftingStamina))]
+        public static void GetMaxHeroCraftingStamina(ref int __result, ref CraftingCampaignBehavior __instance, Hero hero)
+        {
+            if (hero == Hero.MainHero)
+                __result = 1000;
+        }
+    }
 }
